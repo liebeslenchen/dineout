e8e1a1c29d75fefaf5f84cc50391af41ce6b28a6
liebeslenchen <lr878249@fh-muenster.de>
2012-12-14 11:44:11 +0100

    Initalisierung von dineOut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dineout::Application.config.secret_token = '5b45e31ea58aeb5410dfb2e3e0fcc413da7b0a1f4f97f44e25236d5d35c546f9fe2e15a7b81b09d09c10d3818416c890a4ea6569483946c9e7edc06ad3132422'
