b8b8ba2ffc0974fc74c2ebc30edc098104549451
Marlen Rotert <marlen_rotert@web.de>
2012-12-14 13:49:08 +0100

    diner validation hinzugefügt

--- app/models/diner.rb
+++ app/models/diner.rb
@@ -1,3 +1,6 @@
 class Diner < ActiveRecord::Base
   attr_accessible :city, :name, :number, :phone, :street, :time, :url, :urlmaps, :zipcode
+
+  validates_presence_of :name, :city, :number, :street, :zipcode
+  validates :urlmaps, format: %r|^http(s?)://maps.google.de/|
 end
