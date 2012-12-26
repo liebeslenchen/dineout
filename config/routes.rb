5968f2464b8998602cfb28b9cb515bfa08b008aa
liebeslenchen <lr878249@fh-muenster.de>
2012-12-26 10:46:51 +0100

    Einführung von Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Dineout::Application.routes.draw do
+  devise_for :users
+
   resources :events
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121219115013) do
+ActiveRecord::Schema.define(:version => 20121226094520) do
 
   create_table "diners", :force => true do |t|
     t.string   "name"
@@ -35,5 +35,23 @@ ActiveRecord::Schema.define(:version => 20121219115013) do
   end
 
   add_index "events", ["diner_id"], :name => "index_events_on_diner_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
