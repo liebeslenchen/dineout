8a2ff62d1167d1f4ab98c15ac5cac49505928d18
liebeslenchen <lr878249@fh-muenster.de>
2012-12-27 14:14:28 +0100

    Mini-Erweiterung der Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Diner.create city: 'Muenster', name: 'Joeys - Sued West', number: '25', phone: '0251-539300', street: 'Hammer Strasse', time: 'daily from 11h-23h' , url: 'http://www.joeys.de/muenster', urlmaps: 'http://maps.google.de/', zipcode: '48145'
-Diner.create city: 'Muenster', name: 'Joeys - Nord', number: '25', phone: '0251-539300', street: 'Hammer Strasse', time: 'daily from 11h-23h' , url: 'http://www.joeys.de/muenster', urlmaps: 'http://maps.google.de/', zipcode: '48145'
-Diner.create city: 'Muenster', name: 'Berlin-Doener', number: '25', phone: '0251-539300', street: 'Hammer Strasse', time: 'daily from 11h-23h' , url: 'http://www.joeys.de/muenster', urlmaps: 'http://maps.google.de/', zipcode: '48145'
+Diner.create city: 'Muenster', name: 'Joeys - Sued West', number: '25', phone: '0251-539300', street: 'Hammer Strasse', time: 'taeglich from 11h-23h' , url: 'http://www.joeys.de/muenster', urlmaps: 'http://maps.google.de/', zipcode: '48145'
+Diner.create city: 'Muenster', name: 'Papa George', number: '51', phone: '0251-664263', street: 'Wolbecker Strasse', time: 'taeglich from 11h-0h' , url: 'http://papageorge.ms/Imbiss/index.html', urlmaps: 'https://maps.google.de/maps?q=Wolbecker+Stra%C3%9Fe+51,+M%C3%BCnster&hl=de&ie=UTF8&sll=51.950085,7.624097&sspn=0.287359,0.615921&oq=Wolbecker+Stra%C3%9Fe+51&hnear=Wolbecker+Stra%C3%9Fe+51,+48155+M%C3%BCnster,+Nordrhein-Westfalen&t=m&z=16', zipcode: '48155'
+Diner.create city: 'Muenster', name: 'Mocca D'"'or", number: '14-16', phone: '0251-664263', street: 'Rothenburg', time: 'taeglich from 9-1h' , url: 'http://www.mocca-d-or.de/', urlmaps: 'https://maps.google.de/maps?q=Rothenburg+14-16,+48143+M%C3%BCnster&hl=de&ie=UTF8&sll=51.953867,7.639747&sspn=0.017959,0.038495&hnear=Rothenburg+14,+48143+M%C3%BCnster,+Nordrhein-Westfalen&t=m&z=16', zipcode: '48143'
+
