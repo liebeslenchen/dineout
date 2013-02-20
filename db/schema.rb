2c93d8de073bc74230abe2236b35f7f287aa0337
Marlen Rotert <marlen_rotert@web.de>
2013-02-20 21:25:13 +0100

    Benutzername in Devise benutzen und Account ändern

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130118122811) do
+ActiveRecord::Schema.define(:version => 20130220191927) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(:version => 20130118122811) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
